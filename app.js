83fa623202a7c0f389aa6a15e375fccbabebb293
ayoon2 <aaron.yoon2@t-mobile.com>
2021-10-08 12:06:46 -0600

    stackoverflow

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var urlParser = bodyParser.urlencoded();
 
 router.get('/', (req, res, ) => {
   // Returns a list of books in the library
-  res.status(200).json(library.books);
+  res.status(200).send([library.books]);
 });
 
 router.post('/', jsonParser, (req,res) => {
