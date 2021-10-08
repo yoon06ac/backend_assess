8fd702538aa7ff6e7e398f137fb5925ceabe2b93
ayoon2 <aaron.yoon2@t-mobile.com>
2021-10-08 08:16:38 -0600

    pushing first changes

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 const express = require("express");
-const app = express();
-
+const router = express.Router();
 // app.use(express.json());
 // app.use(express.urlencoded({ extended: true }));
+let library = {books:[]}
 
-app.get("/health", (req, res) => {
-  res.status(200).send("Don't panic.");
+router.get('/', (req, res) => {
+  // Returns a list of books in the library
+  res.status(200).json(library.books);
+});
+
+router.post('/', (req,res) => {
+  // Add new book
+  var newBooks = req.body.books;
+  library.books.push(newBooks);
+  res.status(201).send()
+});
+
+router.delete('/', (req,res) => {
+  // Remove all books
+  libary = {books:[]}
+  res.status(204).send()
 });
 
 
-
-module.exports = app;
+module.exports = router;
--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const port = 5000;
-const app = require('./app');
+const router = require('./app');
+const express = require("express");
+const app = express();
 
+app.use('/api/books', router);
+console.log(app.routes);
 app.listen(port, () => {
   console.log(`Server is running, listening on port ${port}`);
 });
