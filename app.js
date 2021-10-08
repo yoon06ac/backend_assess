7f927ad330a564ef51398ac63b6c28603f5ac62f
ayoon2 <aaron.yoon2@t-mobile.com>
2021-10-08 11:00:19 -0600

    debug: printing requests

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const router = express.Router();
+
 // app.use(express.json());
 // app.use(express.urlencoded({ extended: true }));
 let library = {books:[]}
 let id = 0
+
+var jsonParser = bodyParser.json();
+
 router.get('/', (req, res, ) => {
   console.log(library);
   // Returns a list of books in the library
   res.status(200).json(library.books);
 });
 
-router.post('/', (req,res) => {
+router.post('/', jsonParser, (req,res) => {
   console.log(library);
-  console.log('requests', req.body);
+  console.log('request ', req);
   // Add new book
   var newBooks = req.body.books;
   var errMsg = '';
