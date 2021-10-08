02ab54f8d2b0885fb93aa2982b7c1e65bde58482
ayoon2 <aaron.yoon2@t-mobile.com>
2021-10-08 10:36:22 -0600

    removed forward slash in endpoint

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const router = require('./app');
 const express = require("express");
 const app = express();
 
-app.use('/api/books', router);
+app.use('api/books', router);
 console.log(router.stack);
 app.listen(process.env.PORT || 5000, () => {
   console.log(`Server is running, listening on port ${port}`);
